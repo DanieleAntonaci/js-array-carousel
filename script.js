9937ff7043ac73c1cb782432524bd9e7cace6f2b
DanieleAntonaci <danieleanto96@gmail.com>
2022-11-11 17:02:35 +0100

    crfeazione dell' array

--- script.js
+++ script.js
@@ -2,43 +2,55 @@ let btnBefore = document.querySelector('.btn.top');
 let btnAfter = document.querySelector('.btn.bottom');
 let items = document.getElementsByClassName('box-img');
 let activeItems = 0;
-
-btnAfter.addEventListener('click', function () {
-
-    if (activeItems < items.length - 1) {
-        items[activeItems].classList.remove('active');
-
-        activeItems++;
-
-        items[activeItems].classList.add('active');
-        if (activeItems === items.length - 1) {
-            btnAfter.classList.add('hidden')
-        }
-        if (activeItems == 0) {
-            btnBefore.classList.add('hidden')
-        } else if (activeItems > 0) {
-            btnBefore.classList.remove('hidden')
-        }
-
-    }
-});
+const ul = document.querySelector('ul');
+let slide = document.getElementById('slide');
+let arrayImg = [];
+let content;
 
 
-btnBefore.addEventListener('click', function () {
+for (let i = 0; i < 5; i++) {
+    content = `<li class='box-img'><img src="img/0${i + 1}.webp"></li>`
 
-    if (activeItems > 0) {
-        btnBefore.classList.remove('hidden')
-        items[activeItems].classList.remove('active');
+    arrayImg.push(content)
+};
+// arrayImg[0].classList.add('active')
 
-        activeItems--;
+// btnAfter.addEventListener('click', function () {
 
-        items[activeItems].classList.add('active');
+//     if (activeItems < arrayImg.length - 1) {
+//         arrayImg[activeItems].classList.remove('active');
 
-        if (activeItems == 0) {
-            btnBefore.classList.add('hidden')
-        }
-        if (activeItems < items.length - 1) {
-            btnAfter.classList.remove('hidden')
-        }
-    }
-})
+//         activeItems++;
+
+//         items[activeItems].classList.add('active');
+//         if (activeItems === items.length - 1) {
+//             btnAfter.classList.add('hidden')
+//         }
+//         if (activeItems == 0) {
+//             btnBefore.classList.add('hidden')
+//         } else if (activeItems > 0) {
+//             btnBefore.classList.remove('hidden')
+//         }
+
+//     }
+// });
+
+
+// btnBefore.addEventListener('click', function () {
+
+//     if (activeItems > 0) {
+//         btnBefore.classList.remove('hidden')
+//         items[activeItems].classList.remove('active');
+
+//         activeItems--;
+
+//         arrayImg[activeItems].classList.add('active');
+
+//         if (activeItems == 0) {
+//             btnBefore.classList.add('hidden')
+//         }
+//         if (activeItems < arrayImg.length - 1) {
+//             btnAfter.classList.remove('hidden')
+//         }
+//     }
+// })
